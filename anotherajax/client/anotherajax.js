d8aa2a5cdcd7c8e1dc7f0c1d12ba70c876f4bfbe
Sheesh Mohsin <sheeshmohsin@gmail.com>
2014-04-03 15:44:12 +0530

    added 1st commit of 4th day meteor practice

--- anotherajax/client/anotherajax.js
+++ anotherajax/client/anotherajax.js
@@ -1,6 +1,7 @@
  Projects = new Meteor.Collection('projects');
  if (Meteor.isClient) {
  	Session.setDefault('appName', 'Project Manager');
+  Session.setDefault('showProjectDialog', false);
   Meteor.Router.add({
 	'/':'homepage',
 	'/projects':'projects',
@@ -19,6 +20,22 @@
   }
   Template.projects.projectList = function(){
   	return Projects.find()
+  }
+  Template.projectForm.events({
+    'click .save':function(evt, tmpl){
+
+    },
+    'click .cancel':function(evt, tmpl){
+      Session.set('showProjectDialog', false);
+    }
+  })
+  Template.projects.events({
+    'click .addProject':function(evt, tmpl){
+      Session.set('showProjectDialog', true);
+    }
+  })
+  Template.projects.showProjectDialog = function(){
+    return Session.get('showProjectDialog');
   }
 }
 
